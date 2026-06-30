b86e80b4432cd630c04ab20398e41002d116bbb3
Nandini-Ninave <nandinininave99@gmail.com>
2026-06-30 13:53:41 +0530

    Updated state using state updation function

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 // import A from "./A"
 
+import { useState } from "react"
+
 // function App(){
 //   // const x:string[] =  ["hello", "world"]
 //   // const obj:{name:string, age:number} = {name:"xyz", age:9}
@@ -17,16 +19,27 @@
 
 //Conditional rendering
 
-function App(){
-  let num:number = 10
+// function App(){
+//   let num:number = 10
 
-  return(<div>
-    {/* {if(num > 5){
-      return <p>{num}</p>
-    }
-    return <p>less than 10</p>
-    } */}
-    {/* {num>=5?<p>{num}</p>:<p>{num} Less than 5</p>} */}
-  </div>)
+//   return(<div>
+//     {/* {if(num > 5){
+//       return <p>{num}</p>
+//     }
+//     return <p>less than 10</p>
+//     } */}
+//     {/* {num>=5?<p>{num}</p>:<p>{num} Less than 5</p>} */}
+//   </div>)
+// }
+// export default App
+
+function App(){
+  const [username, setuname] = useState("abc")
+  return(
+    <div>
+      <p>{username}</p>
+      <button onClick={()=>setuname("xyz")}></button>
+    </div>
+  )
 }
 export default App
