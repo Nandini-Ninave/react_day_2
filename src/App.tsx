02e4b8b8a71e41d18016c0854ad7223180627a6d
Nandini-Ninave <nandinininave99@gmail.com>
2026-06-30 13:49:44 +0530

    Revert "Added code for usestate hook"
    This reverts commit 500ae418f1f2a102a00bbbc214694b9ea6cae5b9.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 // import A from "./A"
 
-import { useState } from "react"
-
 // function App(){
 //   // const x:string[] =  ["hello", "world"]
 //   // const obj:{name:string, age:number} = {name:"xyz", age:9}
@@ -19,28 +17,16 @@ import { useState } from "react"
 
 //Conditional rendering
 
-// function App(){
-//   // let num:number = 10
-
-//   return(<div>
-//     {/* {if(num > 5){
-//       return <p>{num}</p>
-//     }
-//     return <p>less than 10</p>
-//     } */}
-//     {/* {num>=5?<p>{num}</p>:<p>{num} Less than 5</p>} */}
-//   </div>)
-// }
-// export default App
-
-//usestate
 function App(){
-  const [username, setuname] = useState("abc")
-  return(
-    <div>
-      <p>{username}</p>
-      <button onClick={()=>setuname("xyz")}></button>
-    </div>
-  )
+  let num:number = 10
+
+  return(<div>
+    {/* {if(num > 5){
+      return <p>{num}</p>
+    }
+    return <p>less than 10</p>
+    } */}
+    {/* {num>=5?<p>{num}</p>:<p>{num} Less than 5</p>} */}
+  </div>)
 }
 export default App
